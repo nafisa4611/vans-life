d8679400988e199e82f9cabb9722c2a38b58353c
Nafisa Mila <jabinmila@gmail.com>
2024-12-18 23:00:04 +0600

    Layout added

--- vans-life/src/App.jsx
+++ vans-life/src/App.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
-import Header from './Components/Header/Header';
 import './App.css';
 import Vans from './Components/Vans/Vans';
 import VanDetail from './Components/VanDetail/VanDetail';
+import Layout from './Components/Layout/Layout';
 
 function App() {
   return (
     <div id="root">
-      <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
         </Routes>
       </main>
       <footer className="footer">© 2022 #VANLIFE</footer>
